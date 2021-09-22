03bcbee3c4be7c966a4b90e04343422cda50f772
Darren Reid <darren@reidmail.org>
2021-09-22 17:32:05 +1000

    Use public directory for logo and 'heroImage' frontmatter. Try out some theme config.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,12 @@
 module.exports = {
     title: 'ServiceStack',
     description: 'Docs',
-    themeConfig: {},
+    themeConfig: {
+        repo: 'Layoric/vitepress-starter',
+        editLinks: true,
+        editLinkText: 'Edit this page on GitHub',
+        lastUpdated: 'Last Updated'
+    },
     head: [
 
     ]
